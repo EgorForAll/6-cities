6a40f16927cbed9fd755adc6487bf13a9bb0123d
Егор Иванов <eia.web.ss@gmail.com>
2023-04-29 17:24:14 +0300

    sort menu has added to catalog

--- src/components/main/main-no-empty.jsx
+++ src/components/main/main-no-empty.jsx
@@ -1,7 +1,10 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import CardOffer from "../card-offer/card-offer";
+import SortMenu from "../ui/sort-menu";
 
 const MainNoEmprty = ({offers}) => {
+
+  const [sortMenu, openMenu] = useState(false);
 
   return (
     <main className="page__main page__main--index">
@@ -49,18 +52,13 @@ const MainNoEmprty = ({offers}) => {
             <b className="places__found">{offers.length} places to stay in Amsterdam</b>
             <form className="places__sorting" action="#" method="get">
               <span className="places__sorting-caption">Sort by</span>
-              <span className="places__sorting-type" tabIndex="0">
+              <span className="places__sorting-type" tabIndex="0" onClick={() => openMenu(!sortMenu)}>
                 Popular
                 <svg className="places__sorting-arrow" width="7" height="4">
                   <use xlinkHref="#icon-arrow-select"></use>
                 </svg>
               </span>
-              <ul className="places__options places__options--custom places__options--opened">
-                <li className="places__option places__option--active" tabIndex="0">Popular</li>
-                <li className="places__option" tabIndex="0">Price: low to high</li>
-                <li className="places__option" tabIndex="0">Price: high to low</li>
-                <li className="places__option" tabIndex="0">Top rated first</li>
-              </ul>
+              {sortMenu ? <SortMenu/> : null}
             </form>
             <div className="cities__places-list places__list tabs__content">
               {offers.map((item) => <CardOffer key={item.id} offer={item} />)}
