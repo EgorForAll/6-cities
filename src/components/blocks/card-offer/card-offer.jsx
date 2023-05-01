d5b2d594f2a72a78499d385513eadb6081d96665
Егор Иванов <eia.web.ss@gmail.com>
2023-05-01 17:59:08 +0300

    routing has refactored, file structure has refactored

---
+++ src/components/blocks/card-offer/card-offer.jsx
@@ -0,0 +1,45 @@
+import React, {useState} from "react";
+import PremiumMark from "/src/components/ui/premium-mark";
+import BookmarkSvg from "/src/components/ui/bookmark-svg";
+import {offerValid} from "/src/prop-types/offer";
+import {Link} from "react-router-dom/cjs/react-router-dom.min";
+import {countRating} from "/src/utils/utils";
+
+const CardOffer = ({offer}) => {
+  const [active, setActive] = useState(false);
+
+  return (
+    <article key={offer.id} className="cities__place-card place-card" onMouseEnter={() => setActive(true)} onMouseLeave={() => setActive(false)}>
+      {offer.isPremium ? <PremiumMark /> : null}
+      <div className="cities__image-wrapper place-card__image-wrapper">
+        <a href="#">
+          <img className="place-card__image" src={offer.img} width="260" height="200" alt="Place image" />
+        </a>
+      </div>
+      <div className="place-card__info">
+        <div className="place-card__price-wrapper">
+          <div className="place-card__price">
+            <b className="place-card__price-value">{offer.price} &euro;</b>
+            <span className="place-card__price-text">&#47;&nbsp;night</span>
+          </div>
+          <button className="place-card__bookmark-button button" type="button">
+            <BookmarkSvg offer={offer}/>
+          </button>
+        </div>
+        <div className="place-card__rating rating">
+          <div className="place-card__stars rating__stars">
+            <span style={{width: `${countRating(offer.rating)}%`}}></span>
+            <span className="visually-hidden">Rating</span>
+          </div>
+        </div>
+        <h2 className="place-card__name">
+          <Link to={'/property' + `${offer.id}`}>{offer.description}</Link>
+        </h2>
+        <p className="place-card__type">{offer.type}</p>
+      </div>
+    </article>
+  );
+};
+
+CardOffer.propTypes = offerValid;
+export default CardOffer;
